27619c715e0797e1d15a3ed8b8322add40d81553
endlmk <1269467+endlmk@users.noreply.github.com>
2022-12-12 21:57:17 +0900

    Embed JS in JSX.

--- src/App.jsx
+++ src/App.jsx
@@ -56,13 +56,12 @@ function Gallery() {
 }
 
 function Image() {
+  const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg";
   return (
     <div className="card">
       <div className="card-image">
         <figure className="image">
-          <img
-            src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-            alt="cute dog" />
+          <img src={url} alt="cute dog" />
         </figure>
       </div>
     </div>
