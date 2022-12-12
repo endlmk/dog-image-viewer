156538de6483d19d1f8de0dfb345ca0c8fb29f74
endlmk <1269467+endlmk@users.noreply.github.com>
2022-12-12 21:47:27 +0900

    Devide components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,70 @@
 function App() {
   return (
     <div>
-      <header className="hero is-dark is-bold">
-        <div className="hero-body">
-          <div className="container">
-            <h1>Cute Dog Images</h1>
-          </div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1>Cute Dog Images</h1>
         </div>
-      </header>
-      <main>
-        <section className="section">
-          <div className="container">
-            <div className="column is-vcenterd is-multiline">
-              <div className="column is-3">
-                <div className="card">
-                  <div className="card-image">
-                    <figure className="image">
-                      <img
-                        src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-                        alt="cute dog"
-                      />
-                    </figure>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <p>Dog images are retrived from Dog API</p>
-          <p>
-            <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
-          </p>
+      </div>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Gallery />
         </div>
-      </footer>
+      </section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>Dog images are retrived from Dog API</p>
+        <p>
+          <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+function Gallery() {
+  return (
+    <div className="column is-vcenterd is-multiline">
+      <div className="column is-3">
+        <Image />
+      </div>
+    </div>
+  );
+}
+
+function Image() {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+          <img
+            src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+            alt="cute dog" />
+        </figure>
+      </div>
     </div>
   );
 }
