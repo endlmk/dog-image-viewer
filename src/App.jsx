db3e2b9773aa7a844eadc1af1b6773a483f08e7a
endlmk <1269467+endlmk@users.noreply.github.com>
2022-12-12 22:06:30 +0900

    Repeat images.

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,25 @@ function Header() {
 }
 
 function Main() {
+  const urls = [
+    "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
+  ];
   return (
     <main>
       <section className="section">
         <div className="container">
-          <Gallery />
+          <Gallery urls={urls} />
         </div>
       </section>
     </main>
@@ -45,13 +59,17 @@ function Footer() {
   );
 }
 
-function Gallery() {
-  const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg";
+function Gallery(props) {
+  const { urls } = props;
   return (
-    <div className="column is-vcenterd is-multiline">
-      <div className="column is-3">
-        <Image src={url}/>
-      </div>
+    <div className="columns is-vcenterd is-multiline">
+      {urls.map((url) => {
+        return (
+          <div key={url} className="column is-3">
+            <Image src={url} />
+          </div>
+        );
+      })}
     </div>
   );
 }
